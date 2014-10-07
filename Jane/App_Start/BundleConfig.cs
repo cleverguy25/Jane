a0239f671c8c5621add698ce7bee1b68cf5dab97
cleverguy25 <cleverguy25@hotmail.com>
2014-10-07 19:26:09 +0300

    Basic Foundation check in

--- Jane/App_Start/BundleConfig.cs
+++ Jane/App_Start/BundleConfig.cs
@@ -1,29 +1,25 @@
-﻿// --------------------------------------------------------------------------------------------------------------------
-// <copyright file="BundleConfig.cs" company="Jane OSS">
-//   Copyright (c) Jane Blog Contributors
-// </copyright>
-// --------------------------------------------------------------------------------------------------------------------
+using System.Web;
+using System.Web.Optimization;
+
 namespace Jane
 {
-   using System.Diagnostics.Contracts;
-   using System.Web.Optimization;
+    public class BundleConfig
+    {
+        // For more information on bundling, visit http://go.microsoft.com/fwlink/?LinkId=301862
+        public static void RegisterBundles(BundleCollection bundles)
+        {
+            bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
+                        "~/Scripts/jquery-{version}.js"));
+           
+            // Use the development version of Modernizr to develop with and learn from. Then, when you're
+            // ready for production, use the build tool at http://modernizr.com to pick only the tests you need.
+            bundles.Add(new ScriptBundle("~/bundles/modernizr").Include(
+                        "~/Scripts/modernizr-*"));
 
-   public class BundleConfig
-   {
-      // For more information on bundling, visit http://go.microsoft.com/fwlink/?LinkId=301862
-      public static void RegisterBundles(BundleCollection bundles)
-      {
-         Contract.Requires(bundles != null);
-
-         bundles.Add(new ScriptBundle("~/bundles/jquery").Include("~/Scripts/jquery-{version}.js"));
-
-         bundles.Add(new ScriptBundle("~/bundles/modernizr").Include("~/Scripts/modernizr-*"));
-
-         bundles.Add(new StyleBundle("~/Content/css").Include("~/Content/site.css"));
-
-         // Set EnableOptimizations to false for debugging. For more information,
-         // visit http://go.microsoft.com/fwlink/?LinkId=301862
-         BundleTable.EnableOptimizations = true;
-      }
-   }
+            bundles.Add(new StyleBundle("~/Content/css").Include(
+           "~/Content/site.css"));
+           
+            bundles.Add(Foundation.Scripts());
+        }
+    }
 }
