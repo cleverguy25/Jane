145c356239a2d93748eee68cc02763b0767e6a98
cleverguy25 <cleverguy25@hotmail.com>
2014-10-07 19:47:25 +0300

    Foundation icons

--- Jane/App_Start/Bundles/Foundation.cs
+++ Jane/App_Start/Bundles/Foundation.cs
@@ -2,16 +2,23 @@ using System.Web.Optimization;
 
 namespace Jane
 {
-    public static class Foundation
-    {
-        public static Bundle Scripts()
-        {
-            return new ScriptBundle("~/bundles/foundation").Include(
-                      "~/Scripts/foundation/fastclick.js",
-                      "~/Scripts/jquery.cookie.js",
-                      "~/Scripts/foundation/foundation.js",
-                      "~/Scripts/foundation/foundation.*",
-                      "~/Scripts/foundation/app.js");
-        }
-    }
+   public static class Foundation
+   {
+      public static Bundle Scripts()
+      {
+         return new ScriptBundle("~/bundles/foundation").Include(
+            "~/Scripts/foundation/fastclick.js",
+            "~/Scripts/jquery.cookie.js",
+            "~/Scripts/foundation/foundation.js",
+            "~/Scripts/foundation/foundation.*",
+            "~/Scripts/foundation/app.js");
+      }
+
+      public static Bundle FoundationIcons()
+      {
+         return new StyleBundle("~/Content/Fonts/foundation-icons/css").Include(
+            "~/Content/Fonts/foundation-icons.css",
+            new CssRewriteUrlTransform());
+      }
+   }
 }
