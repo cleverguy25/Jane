7ddc8fe19a91db326d43b60db3daaacdcfdf5717
cleverguy25 <cleverguy25@hotmail.com>
2014-10-09 20:48:01 +0300

    Optimize to remove unneccessary headers

--- Jane/Global.asax.cs
+++ Jane/Global.asax.cs
@@ -5,6 +5,7 @@
 // --------------------------------------------------------------------------------------------------------------------
 namespace Jane
 {
+   using System;
    using System.IO;
    using System.Web;
    using System.Web.Mvc;
@@ -21,12 +22,18 @@ namespace Jane
    {
       protected void Application_Start()
       {
+         MvcHandler.DisableMvcResponseHeader = true;
          AreaRegistration.RegisterAllAreas();
          FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
          RouteConfig.RegisterRoutes(RouteTable.Routes);
          BundleConfig.RegisterBundles(BundleTable.Bundles);
 
          this.SetUpContainer();
+      }
+
+      protected void Application_PreSendRequestHeaders(object sender, EventArgs e)
+      {
+         HttpContext.Current.Response.Headers.Remove("Server");
       }
 
       private void SetUpContainer()
